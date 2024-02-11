6fd0165422ac40fbdd1333b9b9d1f391a5a12449
Ashish-Ranjan001 <106318991+Ashish-Ranjan001@users.noreply.github.com>
2024-02-11 17:56:45 +0530

    updated

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -19,11 +19,13 @@ const uploadOnCloudinary = async (localFilePath) => {
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)//remove the local file
+        return null;
     }
 }
 
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//     { public_id: "olympic_flag" },
+//     function (error, result) { console.log(result); });
 
+export {uploadOnCloudinary}
 
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-    { public_id: "olympic_flag" },
-    function (error, result) { console.log(result); });
